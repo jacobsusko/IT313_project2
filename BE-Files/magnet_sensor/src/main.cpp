05c675920700ff2682afb481a9363b725fd67d19
Siggy <wesley.portlock@gmail.com>
2024-03-04 15:27:46 -0500

    added test magnet files

---
+++ BE-Files/magnet_sensor/src/main.cpp
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+
+int val = 0;  // variable to store hall sensor measurement
+const int halPin = 5;
+#define LED 2
+int counter = 0;
+
+void magnet_detect() {
+    counter++;
+    Serial.println("Detect");
+    Serial.println(counter);
+}
+
+void setup() {
+    Serial.begin(500000);
+    while(Serial.available());
+    pinMode(halPin, INPUT);
+    pinMode(LED, OUTPUT);
+    attachInterrupt(halPin, magnet_detect, RISING);
+    digitalWrite(LED, HIGH);
+}
+
+void loop() {  // Put your main code here to run repeatedly
+    delay(1000);
+    digitalWrite(LED, LOW);
+    int val = digitalRead(halPin);  // variable to store value of hall sensor
+
+}
