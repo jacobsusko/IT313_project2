dfe8f99f599502800f0d2019c19bfddea8bee817
Siggy <wesley.portlock@gmail.com>
2024-04-03 17:28:52 -0400

    Updated Message.cpp

--- BE-Files/ESP32/src/message.cpp
+++ BE-Files/ESP32/src/message.cpp
@@ -183,33 +183,33 @@ void setup() {
   // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
   // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
 
-//   ArduinoOTA
-//     .onStart([]() {
-//       String type;
-//       if (ArduinoOTA.getCommand() == U_FLASH)
-//         type = "sketch";
-//       else // U_SPIFFS
-//         type = "filesystem";
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
 
-//       // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-//       Serial.println("Start updating " + type);
-//     })
-//     .onEnd([]() {
-//       Serial.println("\nEnd");
-//     })
-//     .onProgress([](unsigned int progress, unsigned int total) {
-//       Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-//     })
-//     .onError([](ota_error_t error) {
-//       Serial.printf("Error[%u]: ", error);
-//       if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-//       else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-//       else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-//       else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-//       else if (error == OTA_END_ERROR) Serial.println("End Failed");
-//     });
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
 
-//   ArduinoOTA.begin();
+  ArduinoOTA.begin();
 
   Serial.println("Ready");
   Serial.print("IP address: ");
@@ -234,13 +234,16 @@ void loop() {
   ArduinoOTA.handle();
   // Handle MQTT connection/reconnection
   if (mqtt_server!="") {
+    Serial.print("MQTT Start");
     if (!mqtt.connected()) {
+      Serial.println("MQTT Reconnecting");
       reconnect();
     }
     mqtt.loop();
   }
   // if about mage sensor
-  attachInterrupt(digitalPinToInterrupt(halPin), magnet_detect, RISING);
+  attachInterrupt(digitalPinToInterrupt(halPin), magnet_detect, FALLING);
+  Serial.println("Starting");
   if (interrupt) {
     Serial.println("Interrupt hit");
     int i = 0;
